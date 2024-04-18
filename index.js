d7b6e3deb52830f499d4fcfbe00d05ab2e32ffcc
Mone19 <fuocoangelos@yahoo.de>
2024-04-18 11:52:50 +0200

    Registration Form

---
+++ index.js
@@ -0,0 +1,51 @@
+const express=require("express")
+const bodyParser=require("body-parser")
+const mongoose=require("mongoose")
+
+const app=express()
+
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({
+  extended:true
+}))
+
+mongoose.connect('mongodb://localhost:27017/Database')
+const db=mongoose.connection
+db.on('error',()=>console.log("Error in Connecting to Database"))
+db.once('open',()=> console.log("Connected to Database"))
+
+app.post("/sign_up",(req,res) => {
+  const name = req.body.name
+  const age = req.body.age
+  const email = req.body.email
+  const phone = req.body.phone
+  const gender = req.body.gender
+  const password = req.body.password
+
+  const data = {
+    "name" : name,
+    "age" : age,
+    "email" : email,
+    "phone" : phone,
+    "gender" : gender,
+    "password" : password
+  }
+  db.collection('users').insertOne(data,(err,collection) => {
+    if(err){
+      throw err;
+    }
+    console.log("Record Inserted Successfully")
+  })
+  return res.redirect('signup_successful.html')
+})
+
+app.get("/",(req,res) => {
+  res.set({
+    "Allow-access-Allow-Origin":'*'
+  })
+  return res.redirect('index.html')
+}).listen(3000);
+
+console.log("Listening on port 3000")
+
